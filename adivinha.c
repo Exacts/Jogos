16aa1ced02cd5be6d02369aef8c56dd04724a04f
Exacts Courses <contato@exacts.com.br>
2017-09-06 18:38:54 -0300

    Update adivinha.c

--- adivinha.c
+++ adivinha.c
@@ -1,6 +1,6 @@
 //fale comigo: marcio@exacts.com.br
 /*
- * Programa que sorteia um número para ser adivinhado
+ * Programa que sorteia um número entre 0 e 100 para ser adivinhado em até 10 tentativas
  */
 
 #include <stdio.h>
